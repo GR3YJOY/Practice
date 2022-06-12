437805fa29f0bf550f2b6d73f9a9dc3a4d2f39f5
JOKWIAE <gkdiqkf@naver.com>
2022-06-12 19:59:48 +0900

    집에서연습

---
+++ Book/CH02/P070.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book.CH02
+{
+    internal class P70
+    {
+        
+       
+    }
+}
---
+++ Book/CH02/P095.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book.CH02
+{
+    internal class P095
+    {
+        
+    }
+}
